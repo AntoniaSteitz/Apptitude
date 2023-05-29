eb55996abfb02a7f259fe49786e9353c44ee34fb
Alexandra Saalfeld <alexandra.saalfeld@gmail.com>
2023-05-29 15:57:18 +0200

    created routes & controller skills, added index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'skills/index'
+  get 'skills/new'
+  get 'skills/create'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :skills, only: [:index, :new, :create]
 end
