abffb952b31f7052cef2415c4d5234cb6ad4a10f
Alexandra Saalfeld <alexandra.saalfeld@gmail.com>
2023-05-29 17:01:49 +0200

    added new & create action for skills, missing cloudinary connection

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,8 +4,21 @@ class SkillsController < ApplicationController
   end
 
   def new
+    @skill = Skill.new
   end
 
   def create
+    @skill = Skill.new(skill_params)
+    if @skill.save
+      redirect_to skills_index_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:name, :description, :category, :photo)
   end
 end
