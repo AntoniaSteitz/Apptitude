dd3017ab2b82d0e1c3e87696e3d2e6838914a2df
Darima Pivovarova <dpivovarova@gmail.com>
2023-05-30 17:41:23 +0200

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,31 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts 'Cleaning database'
+Skill.destroy_all
+
 # faking users with faker gem
 10.times do
-  User.create(
+  user =User.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     location: Faker::Address.city,
-    avatar_url: Faker::Avatar.image
+    avatar_url: Faker::Avatar.image,
+    password: "123456"
   )
 end
+puts "users created"
 
 # faking the skills table
 10.times do
-  Skill.create(
+  Skill.create!(
     name: Faker::Hobby.activity,
     description: Faker::Hipster.paragraph,
-    category: ["Language", "Art", "Craftsmanship"].sample
+    category: ['Art', 'Music', 'Craftmanship', 'Language'].sample,
+    user: User.find(rand(1..13))
   )
 end
-
-# Skill.destroy_all
+puts "skills created"
 
 # # categories = ['Art', 'Music', 'Craftmanship', 'Language']
