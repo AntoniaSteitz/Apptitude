c2ba1af36ec339fc786b83bf2d18660168c52a61
Darima Pivovarova <dpivovarova@gmail.com>
2023-06-01 17:39:03 +0200

    requests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,7 @@ class PagesController < ApplicationController
     @bookings = Booking.where(user_id: @user.id)
     @skills = Skill.where(user_id: @user.id)
     # @skill.user = current_user
+    @my_booking = Booking.where(skill_id: @skills)
+    # @booking_requests = current_user.skills.
   end
 end
