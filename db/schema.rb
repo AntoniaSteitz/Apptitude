a7ff4436352eaa8a981937ab3c5a2d03cce6d5e1
Darima Pivovarova <dpivovarova@gmail.com>
2023-06-01 16:22:33 +0200

    intermediate changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_01_122105) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_141248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_01_122105) do
     t.bigint "skill_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.index ["skill_id"], name: "index_bookings_on_skill_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
